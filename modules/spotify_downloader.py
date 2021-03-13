376b8691db07a05bf971c2983e719bc5aad6f167
insaiyancvk <cvamshik1@gmail.com>
2021-03-14 03:28:21 +0530

    wrapped with classes

--- modules/spotify_downloader.py
+++ modules/spotify_downloader.py
@@ -1,113 +1,121 @@
 import json, base64, time, os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from yt_downloader import yt_downloader # type: ignore
+from yt_downloader import yt_downloader # type: ignore     
 
-with open('musicdl/modules/sec.json') as f:
-  data = json.load(f)
+class spotify_downloader():
 
-for key in data.keys():
-    data[key] = base64.b64decode(data[key]).decode('utf8')
+    ytd = yt_downloader()
 
-client_id = data['id']
-client_secret = data['secret']
+    def get_credentials():
+        """ returns a token object after authentication. """
 
-client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        with open('musicdl/modules/sec.json') as f:
+            data = json.load(f)
+        for key in data.keys():
+            data[key] = base64.b64decode(data[key]).decode('utf8')
+        client_id = data['id']
+        client_secret = data['secret']
+        client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+        return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    sp = get_credentials()
 
-def create_dir():
-    """
-    Creates "musicDL downloads" directory on desktop when called. Takes no parameters.
-    """
+    def get_playlist_id(self,url):
+        """ returns the playlist ID given the playlist URI """
 
-    os.chdir(os.path.expanduser("~/Desktop"))
-    try:
-        os.chdir("musicDL downloads")
-    except:
-        os.mkdir("musicDL downloads")
-        os.chdir("musicDL downloads")
+        if "playlist/" in url:
+            ind = url.index("playlist/")
+            return url[ind+9:ind+31]
 
-create_dir()
+    def getTrackIDs(self, user, playlist_id):
+        """ returns IDs of tracks in the playlist as a list given Playlist ID"""
 
-plLink = input("Enter the Spotify playlist URL: ")
-plName = input("Give a name to your playlist: ")
+        ids = []
+        playlist = self.sp.user_playlist(user, playlist_id)
+        for item in playlist['tracks']['items']:
+            track = item['track']
+            ids.append(track['id'])
+        return ids
 
-def get_playlist_id(url):
-    if "playlist/" in url:
-        ind = url.index("playlist/")
-        return url[ind+9:ind+31]
-plID = get_playlist_id(plLink)
+    def getTrackFeatures(self,id):
+        """ returns track name and artist name as a list"""
 
-def getTrackIDs(user, playlist_id):
-    ids = []
-    playlist = sp.user_playlist(user, playlist_id)
-    for item in playlist['tracks']['items']:
-        track = item['track']
-        ids.append(track['id'])
-    return ids
+        meta = self.sp.track(id)
+        name = meta['name']
+        artist = meta['album']['artists'][0]['name']
+        track = [name, artist]
+        return track
 
-trackIDs = getTrackIDs('',plID)
+    def create_PLdir(self,plName):
+        """ creates a playlist directory with the given name """
 
-def getTrackFeatures(id):
-  meta = sp.track(id)
-  features = sp.audio_features(id)
+        try:
+            os.chdir("Playlists")
+        except:
+            os.mkdir("Playlists")
+            os.chdir("Playlists")
+        try:
+            os.chdir(plName)
+        except:
+            os.mkdir(plName)
+            os.chdir(plName)
 
-  name = meta['name']
-  artist = meta['album']['artists'][0]['name']
+    def get_yt_urls(self,tracks):
+        """ returns a list of urls that are fetched successfully and dictionary of urls that were failed to be fetched. """
 
-  track = [name, artist]
-  return track
+        not_fetched = {}
+        urls = []
+        for key in tracks.keys():
+            try:
+                got_url = (key+"+"+tracks[key]).replace(' ','+')
+                urls.append(self.ytd.get_url(got_url)[0])
+            except Exception as e:
+                print(f"\nCould not fetch the link for \"{key} {tracks[key]}\" because: {e}\nmaybe download it as a single")
+                not_fetched[key] = tracks[key]
+        return urls, not_fetched
 
-tracks = {}
-print("\nFetching the details all tracks (name, artist)")
-for i in trackIDs:
-    time.sleep(.5)
-    temp = getTrackFeatures(i)
-    tracks[temp[0]] = temp[1]
-print("Successfully fetched all the track details")
-ytd = yt_downloader()
-urls = []
-# print("\nFetching a download link to each track")
-# print(tracks)
-not_fetched = {}
-for key in tracks.keys():
-    try:
-        if u"\u2018" or u"\u2019" in key+"+"+tracks[key]:
-            got_url = (key+"+"+tracks[key]).replace(' ','+').replace(u"\u2018", "'").replace(u"\u2019", "'")
-            urls.append(ytd.get_url(got_url)[0])
-        else:
-            got_url = (key+"+"+tracks[key]).replace(' ','+')
-            # got_url - urllib.parse.urlsplit(got_url)
-            # got_url = list(got_url)
-            urls.append(ytd.get_url(got_url)[0])
-    except Exception as e:
-        # print(got_url)
-        print(f"\nCould not fetch the link for \"{key} {tracks[key]}\" because: {e}\nmaybe download it as a single")
-        not_fetched[key] = tracks[key]
+    def get_track_details(self,trackIDs):
+        """ returns a dictionary of track name as key and artist name as value given the track ID """
 
-if len(tracks)-len(urls)>0:
-    print(f"\n{len(tracks)-len(urls)}/{len(tracks)} were not fetched")
-    print("\nHere is the list: ")
-    for oh in not_fetched.keys():
-        print(f"{oh} - {not_fetched[oh]}")
-print("\nDownloading the songs\n")
-try:
-    os.chdir("Playlists")
-except:
-    os.mkdir("Playlists")
-    os.chdir("Playlists")
+        tracks = {}
+        for i in trackIDs:
+            time.sleep(.5)
+            temp = self.getTrackFeatures(i)
+            tracks[temp[0]] = temp[1]
+        return tracks
 
-try:
-    os.chdir(plName)
-except:
-    os.mkdir(plName)
-    os.chdir(plName)
+    def download_PL(self,plName, urls):
+        """ downloads all the audio of URLs """
 
-for i in urls:
-    ytd.download_song(i)
+        self.create_PLdir(plName)
+        for i in urls:
+            self.ytd.download_song(i)
 
-total_songs = len(urls)
-downloaded_songs = len(os.listdir())
-if total_songs-downloaded_songs!=0:
-    print(f"\n{total_songs-downloaded_songs}/{total_songs} songs were not downloaded due to some error")
-print(f"\nYour playlist is downloaded in \"/musicDL downloads/Playlists/{plName}\" folder on desktop\n")
+    def interface(self):      
+
+        plLink = input("Enter the Spotify playlist URL: ")
+        plName = input("Give a name to your playlist: ")        
+        plID = self.get_playlist_id(plLink)
+        trackIDs = self.getTrackIDs('',plID)
+        print("\nFetching the details all tracks (name, artist)")
+        tracks = self.get_track_details(trackIDs)
+        print("Successfully fetched all the track details")
+        urls, not_fetched = self.get_yt_urls(tracks)
+
+        #check for track URLs that were failed to be fetched
+        if len(tracks)-len(urls)>0:
+            print(f"\n{len(tracks)-len(urls)}/{len(tracks)} were not fetched")
+            print("\nHere is the list: ")
+            for oh in not_fetched.keys():
+                print(f"{oh} - {not_fetched[oh]}")
+        print("\nDownloading the songs\n")
+
+        # download the tracks
+        self.download_PL(plName, urls)
+
+        # check if all the songs are downloaded
+        total_songs = len(urls)
+        downloaded_songs = len(os.listdir())
+        if total_songs-downloaded_songs!=0:
+            print(f"\n{total_songs-downloaded_songs}/{total_songs} songs were not downloaded due to some error")
+        print(f"\nYour playlist is downloaded in \"/musicDL downloads/Playlists/{plName}\" folder on desktop\n")
