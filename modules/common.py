ef90314ff700842644b28811c0e5efc6fc88577a
insaiyancvk <cvamshik1@gmail.com>
2021-03-19 02:31:45 +0530

    add mp3 conversion

--- modules/common.py
+++ modules/common.py
@@ -3,7 +3,8 @@ try:
 except:
     print("install 'pafy' library with 'pip install pafy'")
 
-import time, os, urllib.request, re 
+import os, urllib.request, re
+from subprocess import Popen, PIPE
 try:
     from youtube_title_parse import get_artist_title 
 except:
@@ -12,8 +13,13 @@ except:
 from urllib.parse import quote
 
 class common():
+    
+    def __init__(self, ffmpeg, alburl, spo=False):
+        self.ffmpeg = ffmpeg
+        self.alburl = alburl
+        self.spo = spo
 
-    def get_url(self,s,n=7):
+    def get_url(self,s):
         """
         Give a video ID as an argument to this function. It returns top n (7 by default) video URLs.
         """
@@ -36,6 +42,20 @@ class common():
                 continue
         return urls
 
+    def convert(self, old, new, v):
+        """ converts any file format to .mp3 with the help of ffmpeg """
+        
+        if self.spo:
+            os.system(self.ffmpeg+' -hide_banner -loglevel quiet -i \"'+old+'\" -b:a 320k \"w'+new+"\"")
+            os.remove(old)
+            urllib.request.urlretrieve(self.alburl, "thumb.png")
+            os.system(self.ffmpeg+' -hide_banner -loglevel warning -i "w'+new+'" -i thumb.png -map 0:0 -map 1:0 -codec copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" "'+new+'"')
+            os.remove("w"+new)
+            os.remove("thumb.png")
+        else:
+            os.system(self.ffmpeg+' -hide_banner -loglevel quiet -i \"'+old+'\" -b:a 320k \"'+new+"\"")
+            os.remove(old)
+
     # Download the song
     def download_song(self,url): 
         """
@@ -48,17 +68,29 @@ class common():
             print(f"\nSome error occurred while fetching the details of the song : {e}\n")
             return
         name = v.title
-        audio = v.getbestaudio(preftype="m4a")
-        print(f"\ndownloading {name} as an audio file")
+        audio = v.getbestaudio()
+        print(f"\n\033[96mdownloading {name} as an audio file\033[97m")
         audio.download()
-        time.sleep(1)
         try:
             artist, title = get_artist_title(name)
             dirs = os.listdir()
             for i in dirs:
                 if name in i:
+                    track_name = title+" - "+artist+".mp3"
+                    print(f"\033[92m Converting \"{i}\" to \"{track_name}\"\033[97m")
+                    self.convert(i, track_name, v)
+
+        except TypeError:
+            for i in dirs:
+                if name in i:
                     ind1 = len(i) - 1 - i[::-1].index('.')
                     ext = i[ind1:]
-                    os.rename(i,title+" - "+artist+ext)
-        except:
-            pass
+                    if ext in i:
+                        print(f"\033[92m Converting \"{i}\" to \"{track_name}\"\033[97m")
+                        track_name = name[:ind1]+".mp3"
+                        self.convert(i, track_name, v)
+                        print("\"\033[92m Successfully Converted \033[97m")
+# ffmpeg = os.getcwd()+"/modules/ffmpeg.exe"
+# os.chdir(os.path.expanduser("~/Desktop/musicDL downloads/test"))
+# cm = common(ffmpeg, alburl='')
+# cm.download_song("https://www.youtube.com/watch?v=tk36ovCMsU8&list=TLPQMTgwMzIwMjGRbAjWbsP1Mw&index=12")
