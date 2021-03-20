2d53c9dcea2235e9f4c11b41a8ed05b2a44c0b09
insaiyancvk <cvamshik1@gmail.com>
2021-03-20 19:02:07 +0530

    change back to musicDL directory after downloading

--- modules/ytDownloader.py
+++ modules/ytDownloader.py
@@ -63,11 +63,12 @@ class yt_downloader():
                 continue
         c = int(input("\nEnter the serial number: "))
 
-        cm.download_song(video_url[c-1],'')
+        cm.download_song(video_url[c-1],'','')
         print("\t","="*100)
         print(f"\n\n\t    Your song is downloaded in \"/musicDL downloads/singles\" folder on desktop\n")
         print("\t","="*100)
         print("\n")
+        os.chdir("..")
     
     def download_playlist(self):
         """
@@ -111,7 +112,7 @@ class yt_downloader():
         print(f"Time taken to fetch the URLs from Youtube: %.2f secs\n"%(end_time-start_time))
         total_songs = len(plLinks)
         for i in plLinks:
-            cm.download_song(i,"")
+            cm.download_song(i,"",'')
         downloaded_songs = len(os.listdir())
         if total_songs-downloaded_songs!=0:
             print(f"\n{total_songs-downloaded_songs}/{total_songs} songs were not downloaded due to some error")
@@ -119,3 +120,4 @@ class yt_downloader():
         print(f"\n\n\t    Your playlist is downloaded in \"/musicDL downloads/Playlists/{plName}\" folder on desktop\n")
         print("\t","="*100)
         print("\n")
+        os.chdir("../..")
