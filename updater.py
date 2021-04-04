9509744bbde7af3b720d26266261f90675a63194
insaiyancvk <cvamshik1@gmail.com>
2021-04-04 13:18:04 +0530

    use wget to download update.zip

--- updater.py
+++ updater.py
@@ -1,5 +1,5 @@
-from io import BytesIO
-import json, requests, urllib, time
+# from io import BytesIO
+import json, requests, wget, time
 from send2trash import send2trash
 import os, shutil, subprocess
 from zipfile import ZipFile
@@ -23,31 +23,34 @@ checkver = req[0]['tag_name']
 details = req[0]['body']
 print("\nHere are the new update details: \n")
 print(details)
-print("\nGetting the redirected download link of the update file\n")
+
+# print("\nGetting the redirected download link of the update file\n")
 j=0
 for i in range(len(req[0]['assets'])):
     if 'update' in req[0]['assets'][i]['name']:
         j=i
-u = urllib.request.urlopen(req[0]['assets'][j]['browser_download_url'])
-url = u.geturl()
+fileurl = req[0]['assets'][j]['browser_download_url']
+# u = urllib.request.urlopen(req[0]['assets'][j]['browser_download_url'])
+# url = u.geturl()
 print("\t\t***** This step could take a while depending on your internet speed *****\n")
 print("Getting details from the redirected link\n")    
 start_time = time.time()
-r = requests.get(
-    url,
-    # headers={'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36'},
-    stream=True,
-    timeout = 10
-).content
+updater = wget.download(fileurl)
+# r = requests.get(
+#     url,
+#     # headers={'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36'},
+#     stream=True,
+#     timeout = 10
+# ).content
 # r = urllib.request.urlopen(urllib.request.Request(
 #     url
 #     # headers={'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36'}
 # )).read()
 end_time = time.time()
-print(f"Time taken to retrieve content of the new update: %.2f secs\n"%(end_time-start_time))
-print("Content of 'update.zip' retrieved\n")
+print(f"\nTime taken to download the new update: %.2f secs\n"%(end_time-start_time))
+# print("Content of 'update.zip' retrieved\n")
 print("Reading the zip file\n")
-f = ZipFile(BytesIO(r))
+f = ZipFile(updater)
 
 a = f.namelist()
 topdir = []
@@ -58,6 +61,7 @@ for i in a:
     else:
         topdir.append(i)
 # print(topdir)
+
 # quit()
 ignore = ['libcrypto-1_1.dll', 'libssl-1_1.dll', 'select.pyd', 'unicodedata.pyd', '_bz2.pyd', '_hashlib.pyd', '_lzma.pyd', '_queue.pyd', '_socket.pyd', '_ssl.pyd']
 existing = os.listdir(cwd)
@@ -80,7 +84,8 @@ print("\nExtracting updated files from 'update.zip'\n")
 for i in a:
     if i not in os.listdir(cwd):
         f.extract(i,cwd)
-
+f.close()
+send2trash(updater)
 version['version'] = checkver
 print("\nUpdating version\n")
 with open(cwd+'version.json', 'w') as f:
