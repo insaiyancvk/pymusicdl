beae11f57a6c15c63bcb388869492f907cd4ef3b
insaiyancvk <cvamshik1@gmail.com>
2021-05-02 15:05:48 +0530

    add check ffmpeg logic

--- musicDL.py
+++ musicDL.py
@@ -1,11 +1,26 @@
-import os, time, subprocess, requests, json, signal, shutil
+import os, subprocess, requests, json, signal, shutil
 from rich.console import Console
 from send2trash import send2trash
 from modules.ytDownloader import yt_downloader 
 from modules.spotify_downloader import spotify_downloader 
 
-ffmpeg = "ffmpeg"
+ffmpeg=""
+def check_ffmpeg():
+    ffmpeg_available = True
+    try:
+        if sys.platform=='linux' or os.name=='posix':
+            subprocess.check_output(['which', 'ffmpeg']) 
+            ffmpeg = "ffmpeg"
+            
+        elif sys.platform=='win32' or os.name=='nt':
+            subprocess.check_output(['where', 'ffmpeg'])
+            ffmpeg="ffmpeg.exe"
 
+    except Exception as e:
+        print(e, e.output)
+        ffmpeg_available = False
+    return ffmpeg_available
+print(ffmpeg)
 def create_dir():
     """
     Creates "musicDL downloads" directory on desktop when called. Takes no parameters.
@@ -19,63 +34,65 @@ def create_dir():
         os.chdir("musicDL downloads")
 
 def main():
-    os.chdir(path)
     
-    # Console().rule(f"\n[green]Your software is running on [cyan] {cver} [/cyan][/green]\n",align="center",style="black")
-    
-    create_dir()
-    print()
-    print(f"\n Enter \n\n 1 - download a song \n 2 - download a YouTube Playlist\n 3 - download from Spotify")    
-    try:
-        ch = int(input("\n  Enter the serial number: "))
-    except ValueError:
-        Console().rule("[red]Invalid input, try 1/2/3[/red]\n",align="left",style="black")
-        main()
-    yt = yt_downloader(ffmpeg)
-    spdl = spotify_downloader(ffmpeg)
-    if ch == 1:
-        try:
-            yt.download_singles()
-        except Exception as e:
-            print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
-            print("Take a screenshort and raise an issue in github or send it to the devs\n")
-            pass
+    if check_ffmpeg():
 
-    elif ch == 2:
+        os.chdir(path)
+        
+        create_dir()
+        print()
+        print(f"\n Enter \n\n 1 - download a song \n 2 - download a YouTube Playlist\n 3 - download from Spotify")    
         try:
-            yt.download_playlist()
-        except Exception as e:
-            print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
-            print("Take a screenshort and raise an issue in github or send it to the devs\n")
-            pass
+            ch = int(input("\n  Enter the serial number: "))
+        except ValueError:
+            Console().rule("[red]Invalid input, try 1/2/3[/red]\n",align="left",style="black")
+            main()
+        yt = yt_downloader(ffmpeg)
+        spdl = spotify_downloader(ffmpeg)
+        if ch == 1:
+            try:
+                yt.download_singles()
+            except Exception as e:
+                print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
+                print("Take a screenshort and raise an issue in github or send it to the devs\n")
+                pass
 
-    elif ch == 3:
-        try:
-            spdl.interface()
-        except Exception as e:
-            print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
-            print("Take a screenshort and raise an issue in github or send it to the devs\n")
-            pass
+        elif ch == 2:
+            try:
+                yt.download_playlist()
+            except Exception as e:
+                print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
+                print("Take a screenshort and raise an issue in github or send it to the devs\n")
+                pass
 
+        elif ch == 3:
+            try:
+                spdl.interface()
+            except Exception as e:
+                print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
+                print("Take a screenshort and raise an issue in github or send it to the devs\n")
+                pass
+
+        else:
+            print("\ninvalid option :(\n")
+            main()
+        n = input("Do you want to continue? (Y/N): ")
+        if n.lower() == 'y':
+            main()
+        else:
+            print("\nSee you later!\n")
     else:
-        print("\ninvalid option :(\n")
-        time.sleep(3)
-        main()
-    n = input("Do you want to continue? (Y/N): ")
-    if n.lower() == 'y':
-        main()
-    else:
-        print("\nSee you later!\n")
-        time.sleep(3)
-        signal.CTRL_C_EVENT
-        Console().print("\n\t** Note: If the app still doesn't [bold]exit, click \"ctrl+c\"[/bold] **\n")
+        print("Please install FFMPEG and add it to your environment variable")  
+        if sys.platform=='win32' or os.name=='nt':
+            print("You can download FFMPEG.exe from insaiyancvk/music_downloader repository and add it to your path")
+        elif sys.platform=='linux' or os.name=='posix':
+            print("Use \n\tsudo apt install ffmpeg\nif you're on debian")
 
 if __name__ == '__main__':
     global path
-    path = os.getcwd()+'\\'
+    path = os.getcwd()+'/'
     Console().rule("\n[bold]Note that you can always quit the program using \"ctrl+c\" shortcut [bold]", style="black", align="center")
     main()
-    signal.CTRL_C_EVENT
 #TODO 1: create playlist and singles directories in musicDL downloads foler ✔
 #TODO 2: Ask user to give a name to the playlist and download the music to that folder ✔
 #TODO 3: make functions for song and playlist downloading ✔
