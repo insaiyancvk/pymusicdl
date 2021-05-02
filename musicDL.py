e0ca012589dd1b72cb422272d90401c806f00dc6
insaiyancvk <cvamshik1@gmail.com>
2021-05-02 17:16:08 +0530

    remove unnecessary code

--- musicDL.py
+++ musicDL.py
@@ -11,7 +11,6 @@ def check_ffmpeg():
             
         if sys.platform=='win32' or os.name=='nt':
             subprocess.check_output(['where', 'ffmpeg'])
-            print(subprocess.check_output(['where', 'ffmpeg']))
 
     except Exception as e:
         print(e)
@@ -47,12 +46,12 @@ def main():
         yt = yt_downloader()
         spdl = spotify_downloader()
         if ch == 1:
-            # try:
-            yt.download_singles()
-            # except Exception as e:
-            #     print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
-            #     print("Take a screenshort and raise an issue in github or send it to the devs\n")
-            #     pass
+            try:
+                yt.download_singles()
+            except Exception as e:
+                print(f"\n\nLooks like something went wrong :(\n{e}\n\n")
+                print("Take a screenshort and raise an issue in github or send it to the devs\n")
+                pass
 
         elif ch == 2:
             try:
@@ -90,19 +89,3 @@ if __name__ == '__main__':
     path = os.getcwd()+'/'
     Console().rule("\n[bold]Note that you can always quit the program using \"ctrl+c\" shortcut [bold]", style="black", align="center")
     main()
-#TODO 1: create playlist and singles directories in musicDL downloads foler ✔
-#TODO 2: Ask user to give a name to the playlist and download the music to that folder ✔
-#TODO 3: make functions for song and playlist downloading ✔
-#TODO 4: implement OOP ✔
-#TODO 5: use a different file for storing all the defined functions  ✔
-#TODO 6: Add spotify support ✔
-#TODO 7: make seperate classes for yt and spotify modules ✔
-#TODO 8: use environment variables for storing API key and secret. {dropped}
-#TODO 9: get dependencies folders into another folder and import from that. (aka beta test) {doesn't work}
-#TODO 10: secure the client ID and Secret. ✔
-#TODO 11: add different colors to different commands (https://ozzmaker.com/add-colour-to-text-in-python/) {just few, not all}
-#TODO 11: convert all the audio files to mp3 or flac based on user choice {mp3 ✔ }{flac dropped}
-#TODO 11: add thumbnails to the downloaded songs (https://www.geeksforgeeks.org/pafy-getting-thumbnail/) ✔
-#TODO 12: release an executable file ✔
-#TODO 13: Make GUI
-#TODO 14: Automatically add the exe file to Windows system variables.
