9131b3f101f9d465eb574f6d8d14bcb185e90cc6
insaiyancvk <cvamshik1@gmail.com>
2021-05-09 01:16:04 +0530

    update init files

--- pymusicdl/__init__.py
+++ pymusicdl/__init__.py
@@ -1,2 +1,7 @@
-from .modules import *
-from .musicDL import main
+from pymusicdl.modules import (
+    common,
+    picker,
+    spotify_downloader,
+    ytDownloader
+)
+from pymusicdl.musicDL import main
