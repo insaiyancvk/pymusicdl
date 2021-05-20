cc98d0d9aae476327f269097bc6f4d5ee17c6296
insaiyancvk <cvamshik1@gmail.com>
2021-05-21 03:22:22 +0530

    complete colab support

--- pymusicdl/__init__.py
+++
@@ -1,7 +0,0 @@
-from pymusicdl.modules import (
-    common,
-    picker,
-    spotify_downloader,
-    ytDownloader
-)
-from pymusicdl.musicDL import main
--- pymusicdl/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from pymusicdl.modules.common import common
-from pymusicdl.modules.spotify_downloader import spotify_downloader
-from pymusicdl.modules.ytDownloader import yt_downloader
-from pymusicdl.modules.picker import *
