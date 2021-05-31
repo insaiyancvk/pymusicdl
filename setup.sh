25022b98521e24614bf9bc8f7c0d4a2b12325756
insaiyancvk <cvamshik1@gmail.com>
2021-06-01 02:09:34 +0530

    update setup shell script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+apt update &&
+apt upgrade &&
 echo "installing python" && 
 pkg install python && 
 echo "successfully installed python" &&
@@ -9,6 +11,7 @@ apt install ffmpeg &&
 echo "successfully installed FFMPEG" &&
 curl -o  ~/../usr/bin/musicdl https://raw.githubusercontent.com/insaiyancvk/pymusicdl/pymusicdl-termux/musicdl && 
 chmod +x  ~/../usr/bin/musicdl && 
+termux-setup-storage &&
 clear && 
 echo -e '\n\nType \033[1m\033[3mmusicdl\033[0m in your terminal to download music :)\n\n' &&
 rm setup.sh
