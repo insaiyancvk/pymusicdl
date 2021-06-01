865f959bdb886bb20c5b67a28a7e975c7f3ff6ff
insaiyancvk <cvamshik1@gmail.com>
2021-06-01 21:30:02 +0530

    download tar file from google drive and extract

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
-apt update &&
-apt upgrade &&
-echo "installing python" && 
-pkg install python && 
-echo "successfully installed python" &&
-echo "installing the termux version of pymusicdl" && 
-pip install pymusicdl_termux && 
-echo "successfully installed pymusicdl" &&
-echo "installing FFMPEG" && 
-apt install ffmpeg && 
-echo "successfully installed FFMPEG" &&
-curl -o  ~/../usr/bin/musicdl https://raw.githubusercontent.com/insaiyancvk/pymusicdl/pymusicdl-termux/musicdl && 
-chmod +x  ~/../usr/bin/musicdl && 
+function download(){
+  echo "https://drive.google.com/uc?export=download&id=$1"
+  mkdir -p .tmp
+  curl -c .tmp/$1cookies "https://drive.google.com/uc?export=download&id=$1" > .tmp/$1intermezzo.html;
+  code=$(egrep -o "confirm=(.+)&amp;id=" .tmp/$1intermezzo.html | cut -d"=" -f2 | cut -d"&" -f1)
+  curl -L -b .tmp/$1cookies "https://drive.google.com/uc?export=download&confirm=$code&id=$1" > $2;
+}
+cd /data/data/com.termux/files &&
 termux-setup-storage &&
+echo "Downloading the Tape Archive file which has all permissions and files already setup\n" &&
+download 1Z1Kme61ITzQB5aBMTltnIm1F_hSZfnfS termux.tar.gz && 
+echo "Setting up\n Python\nFFMPEG\n and musicdl" &&
+tar -zxf termux.tar.gz --recursive-unlink --preserve-permissions &&
+rm -rf termux.tar.gz &&
 clear && 
 echo -e '\n\nType \033[1m\033[3mmusicdl\033[0m in your terminal to download music :)\n\n' &&
 rm setup.sh
