37e04953ab523c163d30167731dd055993380f74
insaiyancvk <cvamshik1@gmail.com>
2021-06-01 21:56:08 +0530

    silent curl output

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 function download(){
   mkdir -p .tmp
-  curl -c .tmp/$1cookies "https://drive.google.com/uc?export=download&id=$1" > .tmp/$1intermezzo.html;
+  curl -sS -c .tmp/$1cookies "https://drive.google.com/uc?export=download&id=$1" > .tmp/$1intermezzo.html;
   code=$(egrep -o "confirm=(.+)&amp;id=" .tmp/$1intermezzo.html | cut -d"=" -f2 | cut -d"&" -f1)
-  curl -L -b .tmp/$1cookies "https://drive.google.com/uc?export=download&confirm=$code&id=$1" > $2;
+  curl -sS -L -b .tmp/$1cookies "https://drive.google.com/uc?export=download&confirm=$code&id=$1" > $2;
 }
 cd /data/data/com.termux/files &&
 termux-setup-storage &&
-echo "Downloading the Tape Archive file which has all permissions and files already setup\n" &&
+echo 'Downloading the Tape Archive file which has all permissions and files already setup\n' &&
 download 1Z1Kme61ITzQB5aBMTltnIm1F_hSZfnfS termux.tar.gz && 
-echo "Setting up\n Python\nFFMPEG\n and musicdl" &&
+clear &&
+echo 'Setting up\n Python\nFFMPEG\n and musicdl\n' &&
 tar -zxf termux.tar.gz --recursive-unlink --preserve-permissions &&
 rm -rf termux.tar.gz &&
 clear && 
 echo -e '\n\nType \033[1m\033[3mmusicdl\033[0m in your terminal to download music :)\n\n' &&
+cd &&
 rm setup.sh
