3a532ccfd113e902db1e56aebf842a2818627c97
insaiyancvk <cvamshik1@gmail.com>
2021-10-15 20:23:42 +0530

    update package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md","r") as f:
 setuptools.setup(
     name = 'pymusicdl_termux',
     packages = setuptools.find_packages(),
-    version = '0.1.0',
+    version = '0.1.0.1',
     license='MIT',
     description = 'Download spotify and youtube playlists on Termux',
     long_description=long_description,
