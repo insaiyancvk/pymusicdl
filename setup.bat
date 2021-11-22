f2f838e6a97bfbc7c398d3130bb4877e049650d1
insaiyancvk <cvamshik1@gmail.com>
2021-11-22 17:13:45 +0530

    update setup files

--- setup.bat
+++ setup.bat
@@ -7,7 +7,12 @@ try:
 except:
     subprocess.call(['py', '-m', 'pip', 'install', 'requests'])
     import requests
-    
+
+try:
+    import pafy
+except:
+    subprocess.call([f'{sys.executable}', '-m', 'pip', 'install', 'git+https://github.com/mps-youtube/pafy.git#egg=pafy'])
+
 try:
     import pymusicdl.musicDL
 except ImportError:
@@ -60,5 +65,6 @@ if "musicdl.bat" not in os.listdir():
     mdl = open(r"musicdl.bat","w+")
     mdl.write('@echo off & py -x "%~f0" %* & goto :eof\n\nfrom pymusicdl.musicDL import main, check_ffmpeg\n\nif check_ffmpeg():\n    main()')
     mdl.close()
+    os.system("cls")
     print("Type 'musicdl' in cmd to download music :)")
 input("press any key to exit")
